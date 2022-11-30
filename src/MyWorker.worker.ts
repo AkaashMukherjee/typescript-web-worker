9284dc59f46b94a01ca47b4f90dd0349b337356d
akaash@jonar.com <akaash@jonar.com>
2022-11-30 17:57:49 -0500

    commented web3 code

--- src/MyWorker.worker.ts
+++ src/MyWorker.worker.ts
@@ -1,10 +1,10 @@
 import EthCrypto from "eth-crypto"
-import Web3 from "web3"
+//import Web3 from "web3"
 
 declare const self: DedicatedWorkerGlobalScope;
 export default {} as typeof Worker & { new (): Worker };
 
-console.log(Web3.name)
+//console.log(Web3.name)
 
 //Test that it's possible to import external library other than Web3 in the worker thread
 console.log('eth public key: ' + EthCrypto.publicKeyByPrivateKey('8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f'))
