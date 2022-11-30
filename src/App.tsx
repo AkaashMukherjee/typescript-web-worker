deac777a0a87263a3bb53a5459326e5795a3c12d
akaash@jonar.com <akaash@jonar.com>
2022-11-30 17:37:16 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import MyWorker from './MyWorker.worker';
-import MyComlinkWorker, { api } from './MyComlinkWorker.worker';
-import { wrap } from 'comlink';
 
 // Example: Using workers natively, e.g. by using "postMessage()"
 const myWorkerInstance: Worker = new MyWorker();
 console.log('[App] MyWorker instance:', myWorkerInstance);
 myWorkerInstance.postMessage('This is a message from the main thread!');
 
-// Example: Using workers via Comlink (comparable to remote execution)
-const myComlinkWorkerInstance: Worker = new MyComlinkWorker();
-const myComlinkWorkerApi = wrap<typeof api>(myComlinkWorkerInstance);
-console.log('[App] MyComlinkWorker instance:', myComlinkWorkerInstance);
-myComlinkWorkerApi.createMessage('John Doe').then((message: string): void => {
-  console.log('[App] MyComlinkWorker message:', message);
-});
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=>{console.log('test')}}>test that main thread is available</button>
     </div>
   );
 }
--- src/MyWorker.worker.ts
+++ src/MyWorker.worker.ts
@@ -1,5 +1,19 @@
+import EthCrypto from "eth-crypto"
+import Web3 from "web3"
+
 declare const self: DedicatedWorkerGlobalScope;
 export default {} as typeof Worker & { new (): Worker };
+
+console.log(Web3.name)
+
+//Test that it's possible to import external library other than Web3 in the worker thread
+console.log('eth public key: ' + EthCrypto.publicKeyByPrivateKey('8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f'))
+
+// create long looping function to test that worker thread is actually running
+ for (let index = 0; index < 500000000; index++) {
+    Math.random()
+ }
+
 
 console.log('[MyWorker] Running.');
 
